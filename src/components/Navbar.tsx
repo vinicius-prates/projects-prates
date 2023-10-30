2190630ad08e862e73b5e0535698bdfff0cf1962
vinicius-prates <viniprates2211@gmail.com>
2023-10-30 14:13:48 -0300

    supabase connect

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,8 @@
 export const Navbar = () => {
-    return(
-        <div className="bg-[#1F1E30] font-bold text-[#B5E67B] text-2xl px-6 py-4 text-center border-b-2 border-[#2C2B4D]  ">CV Projects</div>
-    )
-}
+  return (
+    <div className="flex flex-row justify-between items-center bg-[#1F1E30] font-bold text-[#B5E67B] text-2xl px-6 py-8 text-center border-b-2 border-[#2C2B4D]  ">
+      <h1>Projects</h1>
+      <h1 className="text-sm text-white opacity-40 font-normal">Vinicius Prates</h1>
+    </div>
+  );
+};
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,5 +1,35 @@
+import { useEffect, useState } from "react";
+import { supabase } from "../supabaseClient";
+import { Card } from "./Card";
+export type ProjectType = {
+  id: string;
+  name: string;
+  description: string;
+  leader: string;
+  status: number;
+  created_at: string;
+};
 export const Projects = () => {
+   useEffect(() => {
+    fetchProjects()
+   },[]) 
+  const [projects, setProjects] = useState<ProjectType[]>([]);
+  const fetchProjects = async () => {
+    const { data: Projects, error } = await supabase.from("projects").select();
+
+    if (error) {
+      console.log(`Error: ${error.message}, Code: ${error.code}`);
+    } else {
+      setProjects(Projects!);
+      console.log(Projects);
+    }
+  };
+  if(!projects){
     return(
-        <div className="h-screen bg-[#242337]"></div>
+        <div>lero</div>
     )
-}
+  }else{
+
+      return <div className=" flex flex-col gap-6  bg-[#242337] px-5 py-7"></div>;
+    }
+};
